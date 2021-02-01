be191ffe3cd6c169c98eca5ebd19ba6ed605e447
arturFVictor <arturfvictor22@gmail.com>
2021-02-01 17:34:33 -0300

    implementacao da lista

--- app.ino
+++ app.ino
@@ -1,16 +1,22 @@
 //diagrama UML : https://cdn.discordapp.com/attachments/766307816016838656/804046263267229746/unknown.png
-//implementar lista no codigo
+#include <algorithm>
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <list>
 
+//template < class Type, class Alloc = allocator<T> > class list;
+
 using std::string;
+using std::list; 
 
 class Componente{
   public:
     //Construtor
-    Componente(std::string nome, std::string descricao, int pino);
+    Componente(std::string nome, std::string descricao, int pino){
+      setNome(nome);
+      setDescricao(descricao);
+      setPino(pino);
+}
 
     //Pino
     void setPino(int pino);
@@ -30,12 +36,6 @@ class Componente{
     std::string _nome;
     std::string _descricao;
 };
-
-Componente::Componente(std::string nome, std::string descricao, int pino){
-  setNome(nome);
-  setDescricao(descricao);
-  setPino(pino);
-}
 
 void Componente::setPino(int pino){
   _pino = pino;
@@ -59,6 +59,9 @@ std::string Componente::getDescricao(){
 
 class ComponenteLED: public Componente {
   public:   
+    ComponenteLED(std::string nome, std::string descricao, int pino)
+    :Componente(nome, descricao, pino){
+    };
     void setEstado(bool estadoLED);
     bool getEstado();
 
@@ -74,10 +77,52 @@ bool ComponenteLED::getEstado(){
   return _estadoLED;
 }
 
-class SensorTemperatura: public Componente{
+class ComponenteSensorTemperatura: public Componente{
   public:
+    ComponenteSensorTemperatura(std::string nome, std::string descricao, int pino)
+    :Componente(nome, descricao, pino){};
     float getTemperatura(); 
     
   private:
     float _temperatura;
 };
+
+float ComponenteSensorTemperatura::getTemperatura(){
+  return _temperatura;
+}
+
+void setup(){
+  std::list<Componente> componentes = {};
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(LED_BUILTIN, LOW);
+
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+}
+
+/*
+Rotas
+
+LED
+
+Cadastrar LED
+Remover LED
+Acender/Apagar LED
+Editar LED
+
+SENSOR
+
+Cadastrar SENSOR
+Remover SENSOR
+Pegar Dados SENSOR
+Editar SENSOR
+
+BD
+
+Retorna a lista completa
+*/
